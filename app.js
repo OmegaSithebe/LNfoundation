d4e28e4b3829d60cbf97fba7ed45a32a06397bea
Omega Sithebe <rogersithebe@gmail.com>
2024-09-13 16:03:15 +0200

    13/09/24 Office

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        parent.classList.toggle('active');
+
+        const allItems = document.querySelectorAll('.faq-item');
+        allItems.forEach(otherItem => {
+            if (otherItem !== parent) {
+                otherItem.classList.remove('active');
+            }
+        });
+    });
+});
